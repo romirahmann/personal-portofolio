8451f7bef04c5d73e23effc5e079d56e6d102b40
romirahman <romirahman03romi@gmail.com>
2023-08-19 12:57:37 +0700

    tanpa Backend

--- backend/src/routes/utility_routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const AuthController = require('../../controller/auth_controller/authController')
-
-// auth
-router.post('/login', AuthController.login);
-
-
-module.exports = router;
