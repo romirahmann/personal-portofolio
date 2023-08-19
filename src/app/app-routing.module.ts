bfc8aaf7ff01897e9427a7740e5d28cb365d37e7
romirahman <romirahman03romi@gmail.com>
2023-08-19 12:46:23 +0700

    new template angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LayoutComponent } from './layouts/layout.component';
+
+// Auth
+import { AuthGuard } from './core/guards/auth.guard';
+
+const routes: Routes = [
+  { path: '', component: LayoutComponent, loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule), canActivate: [AuthGuard] },
+  { path: 'auth', loadChildren: () => import('./account/account.module').then(m => m.AccountModule)  },
+  { path: 'pages', loadChildren: () => import('./extraspages/extraspages.module').then(m => m.ExtraspagesModule), canActivate: [AuthGuard] },
+  { path: 'landing', loadChildren: () => import('./landing/landing.module').then(m => m.LandingModule)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
