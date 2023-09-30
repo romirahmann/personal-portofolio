07883142a05e205e694f299e825522df6b553ff2
romirahman <romirahman03romi@gmail.com>
2023-09-30 14:56:54 +0700

    sidebar admin, frontend landing page

--- src/app/pages/frontend/frontend.module.ts
+++ src/app/pages/frontend/frontend.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HeaderComponent } from './component/header/header.component';
 
 
 
 @NgModule({
   declarations: [
-    LandingPageComponent
+    LandingPageComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule
