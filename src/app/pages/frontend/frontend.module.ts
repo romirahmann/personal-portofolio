02ec0b2bad26c9b221e4eb4034ada2374ab52810
romirahman <romirahman03romi@gmail.com>
2023-10-04 10:55:55 +0700

    tampilan service

--- src/app/pages/frontend/frontend.module.ts
+++ src/app/pages/frontend/frontend.module.ts
@@ -1,17 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { HeaderComponent } from './component/header/header.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { LandingPageComponent } from "./landing-page/landing-page.component";
+import { HeaderComponent } from "./component/header/header.component";
+import { MyServiceComponent } from './component/my-service/my-service.component';
 
 @NgModule({
-  declarations: [
-    LandingPageComponent,
-    HeaderComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [LandingPageComponent, HeaderComponent, MyServiceComponent],
+  imports: [CommonModule],
 })
-export class FrontendModule { }
+export class FrontendModule {}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,39 +1,36 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 import {
-  NgbToastModule, NgbProgressbarModule
-} from '@ng-bootstrap/ng-bootstrap';
+  NgbToastModule,
+  NgbProgressbarModule,
+} from "@ng-bootstrap/ng-bootstrap";
 
-import { FlatpickrModule } from 'angularx-flatpickr';
-import { CountToModule } from 'angular-count-to';
-import { NgApexchartsModule } from 'ng-apexcharts';
-import { LeafletModule } from '@asymmetrik/ngx-leaflet';
-import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
-import { SimplebarAngularModule } from 'simplebar-angular';
+import { FlatpickrModule } from "angularx-flatpickr";
+import { CountToModule } from "angular-count-to";
+import { NgApexchartsModule } from "ng-apexcharts";
+import { LeafletModule } from "@asymmetrik/ngx-leaflet";
+import { NgbDropdownModule } from "@ng-bootstrap/ng-bootstrap";
+import { SimplebarAngularModule } from "simplebar-angular";
 
 // Swiper Slider
-import { NgxUsefulSwiperModule } from 'ngx-useful-swiper';
+import { NgxUsefulSwiperModule } from "ngx-useful-swiper";
 
-import { LightboxModule } from 'ngx-lightbox';
+import { LightboxModule } from "ngx-lightbox";
 
 // Load Icons
-import { defineElement } from 'lord-icon-element';
-import lottie from 'lottie-web';
+import { defineElement } from "lord-icon-element";
+import lottie from "lottie-web";
 
 // Pages Routing
 import { PagesRoutingModule } from "./pages-routing.module";
 import { SharedModule } from "../shared/shared.module";
-import { DashboardComponent } from './dashboards/dashboard/dashboard.component';
-import { ToastsContainer } from './dashboards/dashboard/toasts-container.component';
+import { DashboardComponent } from "./dashboards/dashboard/dashboard.component";
+import { ToastsContainer } from "./dashboards/dashboard/toasts-container.component";
 import { DashboardsModule } from "./dashboards/dashboards.module";
 
-
 @NgModule({
-  declarations: [
-    DashboardComponent,
-    ToastsContainer
-  ],
+  declarations: [DashboardComponent, ToastsContainer],
   imports: [
     CommonModule,
     FormsModule,
@@ -49,10 +46,9 @@ import { DashboardsModule } from "./dashboards/dashboards.module";
     NgxUsefulSwiperModule,
     LightboxModule,
     DashboardsModule,
-
   ],
 })
-export class PagesModule { 
+export class PagesModule {
   constructor() {
     defineElement(lottie.loadAnimation);
   }
