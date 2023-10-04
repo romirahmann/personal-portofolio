7bea848bad324030d16a79bb716c41f1ac5da50a
romirahman <romirahman03romi@gmail.com>
2023-10-04 11:50:52 +0700

    animasi

--- src/app/pages/frontend/component/header/header.component.ts
+++ src/app/pages/frontend/component/header/header.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent {
+  constructor() {}
 
+  ngOnInit(): void {
+    this.typeText();
+  }
+
+  typeText() {
+    const text = "Hi There... I'am"; // Ganti dengan teks yang ingin Anda ketikkan
+    let index = 0;
+    const typingText = document.getElementById("sayHi");
+
+    function type() {
+      if (index < text.length) {
+        typingText!.textContent += text.charAt(index);
+        index++;
+        setTimeout(type, 100); // Sesuaikan kecepatan ketik di sini
+      }
+    }
+
+    type();
+  }
 }
